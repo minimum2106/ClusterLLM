6cf9bfe97edf87670708323eb9fd0dea65565a62
minimum2106 <tranhongquan2106@gmail.com>
2024-05-05 15:55:27 +0200

    add create_ouput_folder.sh and update README

---
+++ create_output_folders.sh
@@ -0,0 +1,5 @@
+mkdir perspective/1_predict_triplet/sampled_triplet_results
+mkdir perspective/2_finetune/converted_triplet_results
+mkdir perspective/1_predict_triplet/predicted_triplet_results
+mkdir perspective/2_finetune/checkpoints
+
