54cd946d102998cbad859e6b9e438c7f80d381a2
minimum2106 <tranhongquan2106@gmail.com>
2024-05-05 15:35:41 +0200

    make epoch and iteration parameterizable

--- granularity/scripts/finetune.sh
+++ granularity/scripts/finetune.sh
@@ -1,15 +1,38 @@
-epoch=30
 scale=small
 d="67.0"
 seed=100
 dataset=banking77
+
+
+OPTSTRING=":i:e:"
+
+while getopts ${OPTSTRING} opt; do
+  case ${opt} in
+    i)
+        num_iteration=${OPTARG}
+        ;;
+
+    e)
+        epoch=${OPTARG}
+        ;;
+    
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
 
 # ===== mistral-7b =====
 train_file=granularity/predicted_pair_results/banking77_embed=finetuned_s=small_k=1_multigran2-200_seed=100-mistral_7b-prompts_pair_exps_pair_v3-train.json
 
 CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=4 MKL_NUM_THREADS=4 python granularity/finetune.py \
     --model_name_or_path hkunlp/instructor-large \
-    --output_dir perspective/checkpoints/finetune-pretrain-1024-gpt-noprior/instructor-large-${dataset}-d=${d}-epoch=${epoch}-iter=$1 \
+    --output_dir perspective/checkpoints/finetune-pretrain-1024-gpt-noprior/instructor-large-${dataset}-d=${d}-epoch=${epoch}-iter=$num_iteration \
     --train_file $train_file\
     --cache_dir cache \
     --max_source_length 512 \
