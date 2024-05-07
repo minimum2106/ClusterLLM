c2fd0e21c05bbef372885e556adc852538e79952
minimum2106 <tranhongquan2106@gmail.com>
2024-05-07 12:25:17 +0200

    update run_File.sh

--- run_file.sh
+++ run_file.sh
@@ -46,7 +46,29 @@ do
     bash granularity/scripts/convert_pairs.sh
 
     # Step 5: 
-    bash granularity/scripts/finetune.sh -i $i -e $epoch
+    bash granularity/scripts/finetune.sh -i $num_iteration -e $epoch
 done
+
+for i in $(seq 0 $num_iteration)
+do 
+    # Step 1: Get embeddings from original embedding model
+    # if i == 0 --> run with original embedding model 
+    # if i != 0 --> run with finetuned embedding model of iteration i-1    
+    bash perspective/2_finetune/scripts/get_embedding.sh $i
+
+    # Step 2: Sample triplets
+    bash perspective/1_predict_triplet/scripts/triplet_sampling.sh $i
+
+    # Step 3: Predict triplets
+    bash perspective/1_predict_triplet/scripts/predict_triplet.sh $i
+
+    # Step 4: Convert triplets
+    bash perspective/2_finetune/scripts/convert_triplet.sh
+
+    # Step 5: 
+    bash perspective/2_finetune/scripts/finetune.sh -i $num_iteration -e $epoch
+done
+
+
 
 
